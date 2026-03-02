19fd692984f708892847bf20350765a81f72953b
segFault <github-segfault.discard288@passmail.net>
2026-03-01 22:57:06 -0500

    fix to display projects table under engagement records

--- project-manager-engagement/utility/scripts/dataview/engagement-projects-table.js
+++ project-manager-engagement/utility/scripts/dataview/engagement-projects-table.js
@@ -3,7 +3,7 @@ let currentFileLink = dv.current().file.link;
 dv.table(
 	["Project", "Status", "Priority", "Start Date"],
 	dv.pages()
-        .where(b => b.file.folder.startsWith("projects/") && b.file.tags.includes("#project") && dv.func.contains(b.engagement, currentFileLink))
+        .where(b => b.file.folder === "projects" && b.file.tags.includes("#project") && dv.func.contains(b.engagement, currentFileLink))
 		.sort(b => {
 			const statusOrder = { "New": 1, "Active": 2, "On Hold": 3, "Complete": 4 };
 			return statusOrder[b.status] || 5;
